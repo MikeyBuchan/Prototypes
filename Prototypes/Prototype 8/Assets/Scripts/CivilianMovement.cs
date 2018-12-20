093fbcfdb135c03145165e899b9072b99b096bd4
MikeyBuchan <psmbgames@hotmail.com>
2018-12-20 15:00:31 +0100

    jv

---
+++ Prototypes/Prototype 8/Assets/Scripts/CivilianMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CivilianMovement : MonoBehaviour 
+{
+    public List<GameObject> destination = new List<GameObject>();
+    public int nowAtDestination;
+    public NavMeshAgent agent;
+
+	void Start () 
+	{
+        agent = GetComponent<NavMeshAgent>();
+
+        destination.AddRange(GameObject.FindGameObjectsWithTag("Destination"));
+
+        nowAtDestination = Random.Range(0, destination.Count);
+        agent.SetDestination(destination[nowAtDestination].transform.position);
+    }
+	
+
+	void Update () 
+	{
+
+	}
+
+    public void OnCollisionEnter(Collision c)
+    {
+        if (c.gameObject.tag == "Destination")
+        {
+            nowAtDestination = Random.Range(0, destination.Count);
+            agent.SetDestination(destination[nowAtDestination].transform.position);
+        }
+    }
+}
---
+++ Prototypes/Prototype 8/Assets/Scripts/PlayerInfecting.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfecting : MonoBehaviour 
+{
+    GameObject playerCam;
+    RaycastHit rayHit;
+    public float coughDistance;
+    public bool canCough = true;
+    public float coughReload;
+
+	void Start () 
+	{
+        playerCam = GameObject.FindGameObjectWithTag("MainCamera");
+	}
+	
+
+	void Update () 
+	{
+        SickeningPeople();
+	}
+    
+    public void SickeningPeople()
+    {
+        Debug.DrawRay(playerCam.transform.position, playerCam.transform.forward,Color.red);
+
+        if(Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out rayHit, coughDistance))
+        {
+            if (canCough == true && Input.GetButtonDown("Fire1") && rayHit.transform.tag == "Civilian")
+            {
+                StartCoroutine(CoughReload());
+                rayHit.transform.GetComponent<Sick>().isSick = true;
+            }
+        }
+    }
+
+    public IEnumerator CoughReload()
+    {
+        canCough = false;
+        yield return new WaitForSeconds(coughReload);
+        canCough = true;
+    }
+}
